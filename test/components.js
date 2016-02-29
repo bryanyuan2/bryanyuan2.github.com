0634837e93cd02f7b7a2afb6e60bcdec149ce4d7
Bryan Cheng <cycheng@gmail.com>
2016-02-29 12:38:42 +0800

    fixed folder

--- test/components.js
+++ test/components.js
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactTestUtils = require('react-addons-test-utils');
 //var TestUtils = require('react/lib/ReactTestUtils');
-var compHeader = require("../../js/app/footer.js");
+var compHeader = require("../js/app/footer.js");
 
 var assert = require('chai').assert;
 var expect = require('chai').expect;
