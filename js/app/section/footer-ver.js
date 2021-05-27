13f82deb31d1343badee42dd0bd2279613202c39
Bryan Cheng <bryanyuan2@gmail.com>
2021-05-28 01:50:47 +0800

    dev build

--- js/app/section/footer-ver.js
+++ js/app/section/footer-ver.js
@@ -41,7 +41,7 @@ const FooterContainer = createReactClass({
         return (
             <div id='region-footer'>
                 <hr />
-                <span className='footer-ver'><i>Ver.2021/4/8</i>. </span>
+                <span className='footer-ver'><i>Ver.2021/5/28</i>. </span>
                 Powered by
                 {packages}
                 <br />
