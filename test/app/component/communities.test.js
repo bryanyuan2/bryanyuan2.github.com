ee03d26d42bdc08fa3ea3f0f0795530397bf549b
bryanyuan2 <bryanyuan2@gmail.com>
2025-08-24 21:04:08 +0800

    [dev][copilot] added unittest

---
+++ test/app/component/communities.test.js
@@ -0,0 +1,46 @@
+/*jshint -W030 */
+"use strict";
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var CompCommunities = require("./../../../js/app/section/communities.js");
+var ShallowTestRenderer = require('react-test-renderer/shallow');
+var testingLibraryReact = require('@testing-library/react');
+var expect = require('chai').expect;
+var communitiesJson = require('./../../../data/communities.json');
+
+var reactTestRendererResult;
+var CompCommunitiesElement;
+
+describe('##react-test-renderer## testing', function(){
+  beforeEach(function(done){
+    var shadow = new ShallowTestRenderer();
+    shadow.render(<CompCommunities url={communitiesJson} />);
+    reactTestRendererResult = shadow.getRenderOutput();
+    done();
+  });
+  
+  it('component container should be existed', function () {
+    expect(reactTestRendererResult).to.exist;
+  });
+
+  it('component class should be rendered', function () {
+    expect(reactTestRendererResult.props.id).to.equal('region-communities');
+  });
+
+  it('component title should be rendered', function () {
+    expect(reactTestRendererResult.props.children[0].props.text).to.equal('Communities');
+  });
+});
+
+describe('##react-test-renderer## testing', function(){
+  beforeEach(function(done){
+    CompCommunitiesElement = testingLibraryReact.render(<CompCommunities url={communitiesJson} />);
+    done();
+  });
+  
+  it('title should be rendered', function () {
+    var title = CompCommunitiesElement.getByText(/Communities/).textContent;
+    expect(title).to.equal('Communities');
+  });
+});
---
+++ test/app/component/publications.test.js
@@ -0,0 +1,46 @@
+/*jshint -W030 */
+"use strict";
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var CompPublications = require("./../../../js/app/section/publications.js");
+var ShallowTestRenderer = require('react-test-renderer/shallow');
+var testingLibraryReact = require('@testing-library/react');
+var expect = require('chai').expect;
+var publicationsJson = require('./../../../data/publications.json');
+
+var reactTestRendererResult;
+var CompPublicationsElement;
+
+describe('##react-test-renderer## testing', function(){
+  beforeEach(function(done){
+    var shadow = new ShallowTestRenderer();
+    shadow.render(<CompPublications url={publicationsJson} />);
+    reactTestRendererResult = shadow.getRenderOutput();
+    done();
+  });
+  
+  it('component container should be existed', function () {
+    expect(reactTestRendererResult).to.exist;
+  });
+
+  it('component class should be rendered', function () {
+    expect(reactTestRendererResult.props.id).to.equal('region-publications');
+  });
+
+  it('component title should be rendered', function () {
+    expect(reactTestRendererResult.props.children[0].props.text).to.equal('Publications');
+  });
+});
+
+describe('##react-test-renderer## testing', function(){
+  beforeEach(function(done){
+    CompPublicationsElement = testingLibraryReact.render(<CompPublications url={publicationsJson} />);
+    done();
+  });
+  
+  it('title should be rendered', function () {
+    var title = CompPublicationsElement.getByText(/Publications/).textContent;
+    expect(title).to.equal('Publications');
+  });
+});
