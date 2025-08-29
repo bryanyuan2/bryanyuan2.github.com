c69da1b31a7f6854485b4fb87fa20af9c539f3f0
bryanyuan2 <bryanyuan2@gmail.com>
2025-08-29 15:37:57 +0800

    [dev] migrated class component to functional compoenent

--- src/app/component/presslist.tsx
+++ src/app/component/presslist.tsx
@@ -1,42 +1,37 @@
 import React from 'react';
 
 interface PressListProps {
-    press: Array<{
+    press?: Array<{
         link: string;
         title: string;
         source: string;
     }>;
 }
 
-class PressList extends React.Component<PressListProps> {
-    static defaultProps: Partial<PressListProps> = {
-        press: []
-    };
+const PressList: React.FC<PressListProps> = ({ press = [] }) => {
+    const pressArray: string[] = [];
 
-    render() {
-        const press: string[] = [];
-        let pressContent = '';
-
-        if (this.props.press) {
-            this.props.press.forEach((content) => {
-                press.push(
-                    '<li><a target="_blank" rel="noopener noreferrer" href=' +
-                        content.link +
-                        '>' +
-                        content.title +
-                        ' - ' +
-                        content.source +
-                        '</a></li>'
-                );
-            });
-        }
-        pressContent = press.join(' ');
-        return (
-            <div className="text-press">
-                <div dangerouslySetInnerHTML={{ __html: pressContent }} />
-            </div>
-        );
+    if (press) {
+        press.forEach((content) => {
+            pressArray.push(
+                '<li><a target="_blank" rel="noopener noreferrer" href=' +
+                    content.link +
+                    '>' +
+                    content.title +
+                    ' - ' +
+                    content.source +
+                    '</a></li>'
+            );
+        });
     }
-}
+    
+    const pressContent = pressArray.join(' ');
+    
+    return (
+        <div className="text-press">
+            <div dangerouslySetInnerHTML={{ __html: pressContent }} />
+        </div>
+    );
+};
 
 export default PressList;
--- src/app/component/section-header.tsx
+++ src/app/component/section-header.tsx
@@ -5,20 +5,14 @@ interface SectionHeaderProps {
     text?: string;
 }
 
-class SectionHeader extends React.Component<SectionHeaderProps> {
-    static defaultProps: Partial<SectionHeaderProps> = {
-        setID: '',
-        text: ''
-    };
-
-    render() {
-        const id = this.props.setID ? 'set-' + this.props.setID : 'set';
-        return (
-            <h2 className="text-section" id={id}>
-                {this.props.text}
-            </h2>
-        );
-    }
-}
+const SectionHeader: React.FC<SectionHeaderProps> = ({ setID = '', text = '' }) => {
+    const id = setID ? 'set-' + setID : 'set';
+    
+    return (
+        <h2 className="text-section" id={id}>
+            {text}
+        </h2>
+    );
+};
 
 export default SectionHeader;
